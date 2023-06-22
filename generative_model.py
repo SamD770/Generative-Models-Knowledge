f4c0b7a0eaf6b421cc2c220bd5263f4433916191
SamD770 <samdauncey.sd@gmail.com>
2023-06-22 11:38:19 +0100

    removed name methods from model loading

--- generative_model.py
+++ generative_model.py
@@ -24,23 +24,6 @@ class GenerativeModel:
     def load_serialised(model_name):
         raise NotImplementedError()
 
-    @staticmethod
-    def get_save_file(name):
-        raise NotImplementedError()
-
-    @staticmethod
-    def get_save_dir(name):
-        raise NotImplementedError()
-
-    @staticmethod
-    def get_params(name):
-        raise NotImplementedError()
-
-    @classmethod
-    def load_from_name(cls, name):
-        """Allows for models to be loaded from the unique identifier 'name' strings"""
-        return cls.load_serialised(cls.get_params(name))
-
 
 class AnomalyDetectionMethod:
     """
