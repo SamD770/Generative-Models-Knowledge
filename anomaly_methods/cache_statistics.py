3d91fd352209c345ed0976af80a96c32f41c854c
SamD770 <samdauncey.sd@gmail.com>
2023-06-28 12:13:01 +0100

    cache_statistics script

--- anomaly_methods/cache_statistics.py
+++ anomaly_methods/cache_statistics.py
@@ -1,7 +1,38 @@
 import argparse
+from command_line_utils import anomaly_method_parser, dataset_parser, model_parser
+
+from torch import save
 
 from models.utils import load_generative_model
 from anomaly_methods.utils import anomaly_detection_methods_dict
-from data.utils import to_dataset_wrapper
+from data.utils import get_dataset
 
-#  TODO: command line script for serialising summary statistics
+
+def run(anomaly_method_name, batch_size, model_name, model_type, dataset_names, dataset_split, verbose=True):
+    """Caches the summary statistics from the given anomaly method and model applied to the given datasets."""
+
+    model = load_generative_model(model_type, model_name)
+
+    anomaly_method_name = anomaly_detection_methods_dict[anomaly_method_name]
+
+    anomaly_detector = anomaly_method_name.from_model(model)
+
+    for dataset_name in dataset_names:
+
+        dataset = get_dataset(dataset_name, dataset_split)
+        dataset_summary = anomaly_detector.compute_summary_statistics(dataset, batch_size)
+
+        filepath = anomaly_detector.summary_statistic_filepath(model_name, dataset_name, batch_size)
+
+        if verbose:
+            print("Saving summary statistics to:   ", filepath)
+        save(dataset_summary, filepath)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(parents=[anomaly_method_parser, dataset_parser, model_parser])
+    args = parser.parse_args()
+
+    run(
+        args.anomaly_detection, args.batch_size, args.model_name, args.model_type, args.datasets, args.split
+    )
