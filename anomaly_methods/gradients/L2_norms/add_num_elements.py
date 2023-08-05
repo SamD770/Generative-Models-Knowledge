f3f74d3563f3698d589747faf7f269068b6cf222
SamD770 <s.dauncey@sms.ed.ac.uk>
2023-08-05 23:50:51 +0100

    script to add element counts

--- anomaly_methods/gradients/L2_norms/add_num_elements.py
+++ anomaly_methods/gradients/L2_norms/add_num_elements.py
@@ -6,7 +6,11 @@ import torch
 
 # TODO: abstract this to an "everything generator"
 
-for model_type, model_name_list in [("glow", ["svhn_working"])]:
+for model_type, model_name_list in [
+        # ("glow", ["cifar_long", "celeba", "svhn_working", "imagenet32"]),
+        ("vae", ["VAE_cifar", "VAE_celeba", "VAE_svhn", "VAE_imagenet"]),
+        ("diffusion", ["diffusion_cifar10", "diffusion_celeba", "diffusion_svhn", "diffusion_imagenet32"])
+]:
     for model_name in model_name_list:
 
         model = load_generative_model(model_type, model_name)
@@ -14,7 +18,7 @@ for model_type, model_name_list in [("glow", ["svhn_working"])]:
             name: p.numel() for name, p in model.named_parameters()
         }
 
-        for dataset_name in ["cifar"]:
+        for dataset_name in ["cifar10", "svhn", "celeba", "imagenet32"]:
             for batch_size in [1, 5]:
                 summary_statistic_filename = L2NormAnomalyDetection.summary_statistic_filepath(
                     model_type, model_name, "eval", dataset_name, batch_size
@@ -23,11 +27,18 @@ for model_type, model_name_list in [("glow", ["svhn_working"])]:
                 summary_stats = torch.load(summary_statistic_filename)
                 new_summary_stats = {}
 
+                first_key = iter(summary_stats.keys()).__next__()
+
+                print(first_key)
+
+                if type(first_key) is tuple:
+                    continue
+
                 for key, val in summary_stats.items():
 
                     # The new key is the pair of the parameter name and the number of elements.
                     new_key = (key, numel_dict[key])
-                    print(new_key)
-                    print(val)
-                    exit()
                     new_summary_stats[new_key] = val
+
+                torch.save(new_summary_stats, summary_statistic_filename)
+
