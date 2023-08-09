b2fb39c347b7fe2101dc6e2ddafde8d9104d2f48
SamD770 <s.dauncey@sms.ed.ac.uk>
2023-08-09 15:25:09 +0100

    Plot beautification

--- plots/summary_statistic_histograms/__main__.py
+++ plots/summary_statistic_histograms/__main__.py
@@ -1,3 +1,5 @@
+import random
+
 import numpy as np
 
 from plots.utils import RUNNING_MODULE_DIR, get_dataset_summmaries
@@ -25,8 +27,18 @@ def get_save_dir_path(model_name):
     return save_dir_path
 
 
+def select_summary_stat_names(summary_stat_names, n_statistics):
+    if n_statistics is None:
+        return summary_stat_names
+    else:
+        return random.choices(list(summary_stat_names), k=n_statistics)
+
+
+
 def run(model_type, model_name, model_mode, anomaly_detection_name, batch_size, id_dataset, ood_dataset_names,
-        fitted_distribution):
+        fitted_distribution, n_statistics):
+
+    # Fetch cached statistics from the disk
 
     anomaly_detection_method = anomaly_detection_methods_dict[anomaly_detection_name]
 
@@ -48,15 +60,21 @@ def run(model_type, model_name, model_mode, anomaly_detection_name, batch_size, 
 
     save_dir_path = get_save_dir_path(model_name)
 
-    for stat_name in anomaly_detector.summary_statistic_names:
+    # plot histograms of the data
 
-        title = f"{stat_name} gradient histogram"
+    selected_stat_names = select_summary_stat_names(anomaly_detector.summary_statistic_names, n_statistics)
 
-        filepath = path.join(save_dir_path, title + ".png")
+    for stat_name in selected_stat_names:
+
+        file_title = f"{stat_name} gradient histogram"
+
+        filepath = path.join(save_dir_path, file_title + ".png")
 
         print(f"creating: {filepath}")
+
+        figure_title = f"gradients from 1 layer out of {len(anomaly_detector.summary_statistic_names)} in a {model_type} model"
         fig, ax = plt.subplots()
-        fig.suptitle(title)
+        fig.suptitle(figure_title)
 
         ax.hist(torch.log(id_dataset_summary[stat_name]).numpy(),
                 label=f"in distribution {id_dataset}", density=True, bins=40, alpha=0.6)
@@ -77,17 +95,27 @@ def run(model_type, model_name, model_mode, anomaly_detection_name, batch_size, 
             p = torch.exp(dist.log_prob(x))
             ax.plot(x, p, color="b", label="fitted distribution")
 
-        fig.legend()
+        ax.set_yticks([])
+        ax.set_xlabel("$ \\log f_{\\mathbf{\\theta}_\\ell}(\\mathbf{x}_{1:B})  $")
+
+        ax.legend()
 
         plt.savefig(filepath)
 
 
 parser = argparse.ArgumentParser(parents=[anomaly_method_parser, model_parser, dataset_parser])
+
 parser.add_argument("--fitted_distribution", action="store_true",
                     help="whether plot the fitted distribution, if it exists, "
                          "with the summary statistics (default False)")
 
+parser.add_argument("--n_statistics", type=int, default=None,                       # We need a string here
+                    help="number of statistics to randomly select and plot (defaults to printing all)")
+
 args = parser.parse_args()
 for model_name, id_dataset in zip(args.model_names, args.id_datasets):
     run(args.model_type, model_name, args.model_mode, args.anomaly_detection, args.batch_size,
-        id_dataset, args.datasets, args.fitted_distribution)
+        id_dataset, args.datasets, args.fitted_distribution, args.n_statistics)
+
+
+
