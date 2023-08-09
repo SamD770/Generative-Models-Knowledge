99f5cbf2f4d282d7ca1a083ec5e5220bfe0b6b9f
SamD770 <s.dauncey@sms.ed.ac.uk>
2023-08-09 18:47:50 +0100

    Plot beautification

--- plots/statistic_autocorrelation/__main__.py
+++ plots/statistic_autocorrelation/__main__.py
@@ -13,40 +13,58 @@ import matplotlib.pyplot as plt
 from os import path
 
 from tqdm import tqdm
+import random
 
 
-def run(model_type, model_name, model_mode, anomaly_detection_name, batch_size, id_dataset):
+def compute_autocorr(index, ordered_statistic_names, summary_statistics, maxlag):
+
+    def get_statistics(name_index):
+        statistic_name = ordered_statistic_names[name_index]
+        return torch.log(summary_statistics[statistic_name])
+
+    autocorr = np.zeros(maxlag)
+
+    selected_statistic = get_statistics(index)
+
+    for lag in range(maxlag):
+        index_other = index + lag
+        other_statistic = get_statistics(index_other)
+
+        autocorr[lag] = np.corrcoef(selected_statistic, other_statistic)[0, 1]  # index from correlation matrix
+
+    return autocorr
+
+
+def run(model_type, model_name, model_mode, anomaly_detection_name, batch_size, id_dataset, max_lag=200,
+        n_sampled_layers=200):
     anomaly_detection_method = anomaly_detection_methods_dict[anomaly_detection_name]
 
     id_dataset_summary = load_dataset_summary(model_type, model_name, model_mode,
                                               anomaly_detection_method, batch_size, id_dataset)
 
     # NOTE: this relies on id_dataset_summary.keys() being ordered.
+    # We remove keys with zero standard deviation as this causes zero division errors.
+    ordered_statistic_names = [
+        key for key in id_dataset_summary.keys()
+        if id_dataset_summary[key].std() != 0
+    ]
+    # ordered_statistic_names = list(id_dataset_summary.keys())
+    statistic_indices = range(len(ordered_statistic_names) - max_lag)  # We cut off the tail to prevent overshoot
 
-    stacked_dataset_summary = np.stack([
-        np.array(val) for val in id_dataset_summary.values()
-    ])
+    sampled_statistic_indices = random.choices(statistic_indices, k=n_sampled_layers)
+    total_autocorr = np.zeros(max_lag)
 
-    stacked_dataset_summary = stacked_dataset_summary.T
+    for index in tqdm(sampled_statistic_indices):
+        total_autocorr += compute_autocorr(index, ordered_statistic_names, id_dataset_summary, max_lag)
 
-    print(stacked_dataset_summary.shape)
-
-    autocorr_acc = 0.
-    n_samples = len(stacked_dataset_summary)
-
-    for X in tqdm(stacked_dataset_summary):
-        lags, c, _, _ = plt.acorr(X, maxlags=20)
-        autocorr_acc += c
-
-    autocorr_acc = autocorr_acc/n_samples
-    print(autocorr_acc.shape)
-    print(autocorr_acc)
+    mean_autocorr = total_autocorr/n_sampled_layers
 
     title = f"autocorrelation plot of {anomaly_detection_name} for {model_type} {model_name} evaluated on {id_dataset}"
 
     fig, ax = plt.subplots()
 
-    ax.vlines(lags, ymin=0, ymax=autocorr_acc)
+    lags = range(max_lag)
+    ax.vlines(lags, ymin=0, ymax=mean_autocorr, linewidth=0.5)
 
     ax.set_title(title)
 
