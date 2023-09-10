64801d9ce77413b9e17884a0e96ca8986f7cd676
SamD770 <s.dauncey@sms.ed.ac.uk>
2023-09-10 17:46:01 +0100

    scripts for creating the paper figures

--- figure_scripts/create_pull_figure.sh
+++ figure_scripts/create_pull_figure.sh
@@ -6,4 +6,5 @@ python -m plots.summary_statistic_histograms \
     glow cifar_long svhn_working \
     --id_datasets cifar10 svhn \
     --datasets cifar10 svhn \
-    --anomaly_detection $method --batch_size $bs
+    --anomaly_detection $method --batch_size $bs \
+    --x_lim 0.0 2.0
