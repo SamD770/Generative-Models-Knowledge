4151bb332819ca186167a463c96e3617ee390c85
SamD770 <samdauncey.sd@gmail.com>
2023-09-20 20:44:05 +0100

    generalised to comparing any two methods

--- paper_figure_scripts/runner_tables.sh
+++ paper_figure_scripts/runner_tables.sh
@@ -1,12 +1,13 @@
 
-for bs in 1
+method_1=gradients_L2_norms_gaussian
+method_2=typicality
+
+for bs in 1 5
 do
-  for method in gradients_L2_norms_gaussian
-  do
-    python -m plots.anomaly_table.compare_with_typicality \
-      glow cifar_long celeba svhn_working imagenet32 gtsrb_glow_continued \
-      --id_datasets cifar10 celeba svhn imagenet32 gtsrb \
-      --datasets cifar10 celeba svhn imagenet32 gtsrb \
-      --anomaly_detection $method --batch_size $bs
-  done
+  python -m plots.anomaly_table.compare_methods \
+    glow cifar_long celeba svhn_working imagenet32 gtsrb_glow_continued \
+    --id_datasets cifar10 celeba svhn imagenet32 gtsrb \
+    --datasets cifar10 celeba svhn imagenet32 gtsrb \
+    --anomaly_detection $method_1 --batch_size $bs \
+    --compare_to $method_2
 done
