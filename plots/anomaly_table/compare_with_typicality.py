a429baa785893804dd2ed23771ca76510533f9ed
SamD770 <samdauncey.sd@gmail.com>
2023-09-20 20:34:58 +0100

    working comparison table

--- plots/anomaly_table/compare_with_typicality.py
+++ plots/anomaly_table/compare_with_typicality.py
@@ -1,8 +1,16 @@
-from plots.anomaly_table import get_dataframe, metric_dict
+from plots.anomaly_table import get_dataframe, metric_dict, model_name_formatter, dataset_name_formatter
 from command_line_utils import model_parser, anomaly_method_parser, dataset_parser
 
 import argparse
 import pandas as pd
+
+
+def anomaly_method_name_formatter(anomaly_detection_name):
+    anomaly_detection_name = anomaly_detection_name.replace("_", "\\_")
+
+    # We rotate the anomaly detection name in the table
+    return "\\rotatebox[origin=c]{90}{" + anomaly_detection_name + "}"
+
 
 def run(model_type, model_names, model_mode, batch_size, id_datasets, dataset_names,
         metric_name, model_name_column):
@@ -10,20 +18,20 @@ def run(model_type, model_names, model_mode, batch_size, id_datasets, dataset_na
     method_1 = "typicality"
     method_2 = "gradients_L2_norms_gaussian"
 
-    df_typicality = get_dataframe(method_1, batch_size, dataset_names,
+    df_method_1 = get_dataframe(method_1, batch_size, dataset_names,
                                   id_datasets, metric_name, model_mode, model_names, model_name_column, model_type)
 
-    df_gradients = get_dataframe(method_2, batch_size, dataset_names,
+    df_method_2 = get_dataframe(method_2, batch_size, dataset_names,
                                  id_datasets, metric_name, model_mode, model_names, model_name_column, model_type)
 
-    df_comparison = df_gradients > df_typicality
+    df_comparison = df_method_2 > df_method_1
 
     # A silly little hack to store the float values which we cant to be bold
 
     bold_flag_dict = {
-        performance: greater_than for performance, greater_than in zip(df_gradients.stack(), df_comparison.stack())
+        performance: greater_than for performance, greater_than in zip(df_method_2.stack(dropna=False), df_comparison.stack())
     } | {
-        performance: not greater_than for performance, greater_than in zip(df_typicality.stack(), df_comparison.stack())
+        performance: not greater_than for performance, greater_than in zip(df_method_1.stack(dropna=False), df_comparison.stack())
     }
 
     def bold_formatter(performance):
@@ -36,26 +44,51 @@ def run(model_type, model_names, model_mode, batch_size, id_datasets, dataset_na
         performance = format(performance, ".4f")
 
         if bold_flag:
-            performance = "\\B{" + performance + "}"
+            performance = "\\textbf{" + performance + "}"
 
         return performance
 
-    styler = df_gradients.style
+    df = pd.concat([df_method_1, df_method_2],
+                   keys=[method_1, method_2])
+
+    styler = df.style
+
+    if model_name_column:
+        column_formatter = model_name_formatter
+    else:
+        column_formatter = dataset_name_formatter
+
+    styler = styler.format_index(
+        formatter=column_formatter,
+        axis="columns"
+    )
+
+    styler = styler.format_index(
+        formatter=anomaly_method_name_formatter,
+        axis="index",
+        level=0
+    )
+
+    styler = styler.format_index(
+        formatter=dataset_name_formatter,
+        axis="index",
+        level=1
+    )
 
     styler = styler.format(
         formatter=bold_formatter
     )
 
     table_latex = styler.to_latex(
-      hrules=True,
-      position="H",
-      column_format="l | r r r r r"
+        hrules=True,
+        position="H",
+        clines="skip-last;data",
+        column_format="l | l | r r r r r"
     )
-
 
     print(table_latex)
 
-    df_comparison = (df_gradients > df_typicality)
+    df_comparison = (df_method_2 > df_method_1)
 
 
 
