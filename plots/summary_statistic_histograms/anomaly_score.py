212e89b28438f97be78387b9a8a091d8f5358e22
SamD770 <samdauncey.sd@gmail.com>
2023-10-17 13:27:06 +0100

    interface with new code

--- plots/summary_statistic_histograms/anomaly_score.py
+++ plots/summary_statistic_histograms/anomaly_score.py
@@ -1,9 +1,11 @@
 import matplotlib.pyplot as plt
+import torch
+import numpy as np
 
 from plots.summary_statistic_histograms import plot_summary_histograms
 from command_line_utils import model_parser, anomaly_method_parser, dataset_parser
 
-from plots.utils import get_anomaly_scores
+from plots.utils import get_anomaly_scores, save_plot
 
 import argparse
 
@@ -19,6 +21,18 @@ def run(model_type, model_name, model_mode, anomaly_detection_name, batch_size, 
     id_test_anomaly_scores, all_anomaly_scores_list = \
         get_anomaly_scores(model_type, model_name, model_mode, anomaly_detection_name, batch_size, id_dataset, ood_dataset_names)
 
+    exponents = list(range(1, 6))
+
+    major_ticks = 10**np.array(exponents)
+    minor_ticks = np.outer(major_ticks, np.linspace(1, 10, 10))
+    minor_ticks = minor_ticks.flatten()
+
+    bois = np.log(major_ticks)
+    lil_bois = np.log(minor_ticks)
+
+    lines = list(bois)
+    print(lines)
+
     id_anomaly_scores_summary = to_summary(id_test_anomaly_scores)
     all_anomaly_scores_summary = [
         to_summary(anomaly_scores) for anomaly_scores in all_anomaly_scores_list
@@ -26,8 +40,21 @@ def run(model_type, model_name, model_mode, anomaly_detection_name, batch_size, 
 
     fig, ax = plt.subplots()
 
-    plot_summary_histograms(ax, id_anomaly_scores_summary, id_dataset, all_anomaly_scores_summary, ood_dataset_names, "anomaly_score")
+    plot_summary_histograms(ax, id_anomaly_scores_summary, id_dataset,
+                            all_anomaly_scores_summary, ood_dataset_names, "anomaly_score")
 
+    exponent_labels = [f"$10^{exponent}$" for exponent in exponents]
+
+    ax.set_xticks(bois, labels=exponent_labels)
+    ax.set_xticks(lil_bois, minor=True)
+    ax.set_xlabel("Naive $L^2$ norm of $\\nabla_{\\theta} l(\\mathbf{x})$")
+    ax.set_xlim(1.5 * np.log(10), 4.8 * np.log(10))
+
+    ax.set_yticks([])
+
+    ax.legend()
+
+    save_plot(f"{model_type} {model_name} {anomaly_detection_name} scores")
     print("success")
 
 
